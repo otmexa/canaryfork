71448e475f6b66c9cb5020674e914896ca324e9c
flowbomba <flowbomba@gmail.com>
2024-08-15 22:12:47 -0600

    back original version part 1
    data and data canary

--- src/creatures/players/player.cpp
+++ src/creatures/players/player.cpp
@@ -1,6 +1,6 @@
 /**
  * Canary - A free and open-source MMORPG server emulator
- * Copyright (©) 2019-2024 OpenTibiaBR <opentibiabr@outlook.com>
+ * Copyright (©) 2019-2022 OpenTibiaBR <opentibiabr@outlook.com>
  * Repository: https://github.com/opentibiabr/canary
  * License: https://github.com/opentibiabr/canary/blob/main/LICENSE
  * Contributors: https://github.com/opentibiabr/canary/graphs/contributors
@@ -15,7 +15,6 @@
 #include "creatures/monsters/monsters.hpp"
 #include "creatures/players/player.hpp"
 #include "creatures/players/wheel/player_wheel.hpp"
-#include "creatures/players/achievement/player_achievement.hpp"
 #include "creatures/players/storages/storages.hpp"
 #include "game/game.hpp"
 #include "game/modal_window/modal_window.hpp"
@@ -48,7 +47,6 @@ Player::Player(ProtocolGame_ptr p) :
 	inbox(std::make_shared<Inbox>(ITEM_INBOX)),
 	client(std::move(p)) {
 	m_wheelPlayer = std::make_unique<PlayerWheel>(*this);
-	m_playerAchievement = std::make_unique<PlayerAchievement>(*this);
 }
 
 Player::~Player() {
@@ -2669,11 +2667,6 @@ BlockType_t Player::blockHit(std::shared_ptr<Creature> attacker, CombatType_t co
 }
 
 void Player::death(std::shared_ptr<Creature> lastHitCreature) {
-	if (!g_configManager().getBoolean(TOGGLE_MOUNT_IN_PZ, __FUNCTION__) && isMounted()) {
-		dismount();
-		g_game().internalCreatureChangeOutfit(getPlayer(), defaultOutfit);
-	}
-
 	loginPosition = town->getTemplePosition();
 
 	g_game().sendSingleSoundEffect(static_self_cast<Player>()->getPosition(), sex == PLAYERSEX_FEMALE ? SoundEffect_t::HUMAN_FEMALE_DEATH : SoundEffect_t::HUMAN_MALE_DEATH, getPlayer());
@@ -3182,18 +3175,12 @@ ReturnValue Player::queryAdd(int32_t index, const std::shared_ptr<Thing> &thing,
 	ReturnValue ret = RETURNVALUE_NOERROR;
 
 	const int32_t &slotPosition = item->getSlotPosition();
-
-	bool allowPutItemsOnAmmoSlot = g_configManager().getBoolean(ENABLE_PLAYER_PUT_ITEM_IN_AMMO_SLOT, __FUNCTION__);
-	if (allowPutItemsOnAmmoSlot && index == CONST_SLOT_AMMO) {
-		ret = RETURNVALUE_NOERROR;
-	} else {
-		if ((slotPosition & SLOTP_HEAD) || (slotPosition & SLOTP_NECKLACE) || (slotPosition & SLOTP_BACKPACK) || (slotPosition & SLOTP_ARMOR) || (slotPosition & SLOTP_LEGS) || (slotPosition & SLOTP_FEET) || (slotPosition & SLOTP_RING)) {
-			ret = RETURNVALUE_CANNOTBEDRESSED;
-		} else if (slotPosition & SLOTP_TWO_HAND) {
-			ret = RETURNVALUE_PUTTHISOBJECTINBOTHHANDS;
-		} else if ((slotPosition & SLOTP_RIGHT) || (slotPosition & SLOTP_LEFT)) {
-			ret = RETURNVALUE_CANNOTBEDRESSED;
-		}
+	if ((slotPosition & SLOTP_HEAD) || (slotPosition & SLOTP_NECKLACE) || (slotPosition & SLOTP_BACKPACK) || (slotPosition & SLOTP_ARMOR) || (slotPosition & SLOTP_LEGS) || (slotPosition & SLOTP_FEET) || (slotPosition & SLOTP_RING)) {
+		ret = RETURNVALUE_CANNOTBEDRESSED;
+	} else if (slotPosition & SLOTP_TWO_HAND) {
+		ret = RETURNVALUE_PUTTHISOBJECTINBOTHHANDS;
+	} else if ((slotPosition & SLOTP_RIGHT) || (slotPosition & SLOTP_LEFT)) {
+		ret = RETURNVALUE_CANNOTBEDRESSED;
 	}
 
 	switch (index) {
@@ -3335,12 +3322,8 @@ ReturnValue Player::queryAdd(int32_t index, const std::shared_ptr<Thing> &thing,
 		}
 
 		case CONST_SLOT_AMMO: {
-			if (allowPutItemsOnAmmoSlot) {
+			if ((slotPosition & SLOTP_AMMO)) {
 				ret = RETURNVALUE_NOERROR;
-			} else {
-				if ((slotPosition & SLOTP_AMMO)) {
-					ret = RETURNVALUE_NOERROR;
-				}
 			}
 			break;
 		}
@@ -5797,7 +5780,7 @@ bool Player::toggleMount(bool mount) {
 			return false;
 		}
 
-		uint8_t currentMountId = getCurrentMount();
+		uint8_t currentMountId = getLastMount();
 		if (currentMountId == 0) {
 			sendOutfitWindow();
 			return false;
@@ -6121,12 +6104,6 @@ void Player::sendClosePrivate(uint16_t channelId) {
 
 	if (client) {
 		client->sendClosePrivate(channelId);
-	}
-}
-
-void Player::sendCyclopediaCharacterAchievements(uint16_t secretsUnlocked, std::vector<std::pair<Achievement, uint32_t>> achievementsUnlocked) {
-	if (client) {
-		client->sendCyclopediaCharacterAchievements(secretsUnlocked, achievementsUnlocked);
 	}
 }
 
@@ -7954,15 +7931,6 @@ std::unique_ptr<PlayerWheel> &Player::wheel() {
 
 const std::unique_ptr<PlayerWheel> &Player::wheel() const {
 	return m_wheelPlayer;
-}
-
-// Achievement interface
-std::unique_ptr<PlayerAchievement> &Player::achiev() {
-	return m_playerAchievement;
-}
-
-const std::unique_ptr<PlayerAchievement> &Player::achiev() const {
-	return m_playerAchievement;
 }
 
 void Player::sendLootMessage(const std::string &message) const {
--- src/game/functions/game_reload.cpp
+++ src/game/functions/game_reload.cpp
@@ -1,6 +1,6 @@
 /**
  * Canary - A free and open-source MMORPG server emulator
- * Copyright (©) 2019-2024 OpenTibiaBR <opentibiabr@outlook.com>
+ * Copyright (©) 2019-2022 OpenTibiaBR <opentibiabr@outlook.com>
  * Repository: https://github.com/opentibiabr/canary
  * License: https://github.com/opentibiabr/canary/blob/main/LICENSE
  * Contributors: https://github.com/opentibiabr/canary/graphs/contributors
@@ -61,15 +61,6 @@ uint8_t GameReload::getReloadNumber(Reload_t reloadTypes) const {
 	return magic_enum::enum_integer(reloadTypes);
 }
 
-// Helper function for logging reload status
-void logReloadStatus(const std::string &name, bool result) {
-	if (result) {
-		g_logger().info("Reloaded: {}", name);
-	} else {
-		g_logger().error("Failed to reload: {}", name);
-	}
-}
-
 /*
  * From here down have the private members functions
  * These should only be used within the class itself
@@ -81,123 +72,98 @@ bool GameReload::reloadAll() const {
 	reloadResults.reserve(magic_enum::enum_count<Reload_t>());
 
 	for (auto value : magic_enum::enum_values<Reload_t>()) {
-		const auto name = magic_enum::enum_name(value);
-		g_logger().info("Reloading: {}", name);
-		if (value != Reload_t::RELOAD_TYPE_ALL) {
-			reloadResults.push_back(init(value));
+		g_logger().info("Reloading: {}", magic_enum::enum_name(value));
+		if (value == Reload_t::RELOAD_TYPE_ALL) {
+			continue;
 		}
+
+		reloadResults.push_back(init(value));
 	}
 
 	return std::ranges::any_of(reloadResults, [](bool result) { return result; });
 }
 
 bool GameReload::reloadChat() const {
-	const bool result = g_chat().load();
-	logReloadStatus("Chat", result);
-	return result;
+	return g_chat().load();
 }
 
 bool GameReload::reloadConfig() const {
-	const bool result = g_configManager().reload();
-	logReloadStatus("Config", result);
-	return result;
+	return g_configManager().reload();
 }
 
 bool GameReload::reloadEvents() const {
-	const bool result = g_events().loadFromXml();
-	logReloadStatus("Events", result);
-	return result;
+	return g_events().loadFromXml();
 }
 
 bool GameReload::reloadCore() const {
-	const auto &coreFolder = g_configManager().getString(CORE_DIRECTORY, __FUNCTION__);
-	const bool coreLoaded = g_luaEnvironment().loadFile(coreFolder + "/core.lua", "core.lua") == 0;
-
-	if (coreLoaded) {
-		const auto &datapackFolder = g_configManager().getString(CORE_DIRECTORY, __FUNCTION__);
-		const bool scriptsLoaded = g_scripts().loadScripts(coreFolder + "/scripts/lib", true, false);
-		if (scriptsLoaded) {
-			return true;
+	if (auto coreFolder = g_configManager().getString(CORE_DIRECTORY, __FUNCTION__);
+		g_luaEnvironment().loadFile(coreFolder + "/core.lua", "core.lua") == 0) {
+		// Reload scripts lib
+		auto datapackFolder = g_configManager().getString(DATA_DIRECTORY, __FUNCTION__);
+		if (!g_scripts().loadScripts(datapackFolder + "/scripts/lib", true, false)) {
+			return false;
 		}
-	}
 
-	logReloadStatus("Core", false);
+		return true;
+	}
 	return false;
 }
 
 bool GameReload::reloadImbuements() const {
-	const bool result = g_imbuements().reload();
-	logReloadStatus("Imbuements", result);
-	return result;
+	return g_imbuements().reload();
 }
 
 bool GameReload::reloadItems() const {
-	const bool result = Item::items.reload();
-	logReloadStatus("Items", result);
-	return result;
+	return Item::items.reload();
 }
 
 bool GameReload::reloadModules() const {
-	const bool result = g_modules().reload();
-	logReloadStatus("Modules", result);
-	return result;
+	return g_modules().reload();
 }
 
 bool GameReload::reloadMonsters() const {
+	// Clear registered MonsterType vector
 	g_monsters().clear();
-	const auto &datapackFolder = g_configManager().getString(DATA_DIRECTORY, __FUNCTION__);
-	const auto &coreFolder = g_configManager().getString(CORE_DIRECTORY, __FUNCTION__);
-
-	const bool scriptsLoaded = g_scripts().loadScripts(coreFolder + "/scripts/lib", true, false);
-	const bool monsterScriptsLoaded = g_scripts().loadScripts(datapackFolder + "/monster", false, true);
-
-	if (scriptsLoaded && monsterScriptsLoaded) {
-		logReloadStatus("Monsters", true);
-		return true;
-	} else {
-		logReloadStatus("Monsters", false);
+	// Resets monster spells to prevent the spell from being incorrectly cleared from memory
+	auto datapackFolder = g_configManager().getString(DATA_DIRECTORY, __FUNCTION__);
+	if (!g_scripts().loadScripts(datapackFolder + "/scripts/lib", true, false)) {
 		return false;
 	}
+
+	if (g_scripts().loadScripts(datapackFolder + "/monster", false, true) && g_scripts().loadScripts(datapackFolder + "/scripts/lib", true, true)) {
+		return true;
+	}
+	return false;
 }
 
 bool GameReload::reloadMounts() const {
-	const bool result = g_game().mounts.reload();
-	logReloadStatus("Mounts", result);
-	return result;
+	return g_game().mounts.reload();
 }
 
 bool GameReload::reloadNpcs() const {
-	const bool result = g_npcs().reload();
-	logReloadStatus("NPCs", result);
-	return result;
+	return g_npcs().reload();
 }
 
 bool GameReload::reloadRaids() const {
-	const bool result = g_game().raids.reload() && g_game().raids.startup();
-	logReloadStatus("Raids", result);
-	return result;
+	return g_game().raids.reload() && g_game().raids.startup();
 }
 
 bool GameReload::reloadScripts() const {
 	g_scripts().clearAllScripts();
 	Zone::clearZones();
-
-	const auto &datapackFolder = g_configManager().getString(DATA_DIRECTORY, __FUNCTION__);
-	const auto &coreFolder = g_configManager().getString(CORE_DIRECTORY, __FUNCTION__);
-
-	g_scripts().loadScripts(coreFolder + "/scripts/lib", true, false);
+	// Reset scripts lib to prevent the objects from being incorrectly cleared from memory
+	auto datapackFolder = g_configManager().getString(DATA_DIRECTORY, __FUNCTION__);
+	g_scripts().loadScripts(datapackFolder + "/scripts/lib", true, false);
+	auto coreFolder = g_configManager().getString(CORE_DIRECTORY, __FUNCTION__);
 	g_scripts().loadScripts(datapackFolder + "/scripts", false, true);
 	g_scripts().loadScripts(coreFolder + "/scripts", false, true);
 
 	// It should come last, after everything else has been cleaned up.
 	reloadMonsters();
 	reloadNpcs();
-	logReloadStatus("Scripts", true);
 	return true;
 }
 
 bool GameReload::reloadGroups() const {
-	const bool result = g_game().groups.reload();
-	logReloadStatus("Groups", result);
-	return result;
+	return g_game().groups.reload();
 }
